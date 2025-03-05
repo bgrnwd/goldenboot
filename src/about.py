4d4409dccf92f850edb8f62bd4e2c2ed03120bb9
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2025-03-04 23:59:08 -0500

    feat: add about tab

---
+++ src/about.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("About")
+st.header("History")
+st.markdown("""The [ExtraTime Radio podcast](https://www.mlssoccer.com/video/topics/extratime/) (RIP :headstone:) held the first Golden Boot Draft in 2021.
+[Andrew Wiebe](https://bsky.app/profile/andrew-wiebe.bsky.social), [Calen Carr](https://www.calencarr.com/mystory), [David Gass](https://bsky.app/profile/empiregass.bsky.social), and [Matt Doyle](https://bsky.app/profile/mattdoyle.bsky.social) took part in the inaugural draft by selecting 2 players each.
+In 2022, the team size was increased to 4 players. In 2023, [Tom Bogert](https://bsky.app/profile/tombogert.bsky.social) replaced Calen and each team was allowed to keep one player from the previous year.
+In 2025, Producer Anders and Morgan (Admin) joined in on the fun.
+""")
+st.subheader("Past winners")
+st.markdown("""
+- **2024**: Tom Bogert :medal:
+- **2023**: Tom Bogert :trophy:
+- **2022**: Andrew Wiebe (120 goals) :medal:
+- **2021**: Matt Doyle (23 goals) :trophy:
+""")
+st.header("2025 Rules")
+st.markdown("""
+1. The draft format will be a snake draft
+2. Each team/participant will select 11 players
+3. 1 of those 11 players must be a centerback
+4. Only goals scored during the MLS regular season are counted
+5. Penalties **ARE** counted
+6. Assists will be used as a tiebreaker in the event that more than 1 team ends the season tied on goals
+""")
