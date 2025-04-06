367226b49dfba8fbd7eef32d584d69d05a03c561
bgrnwd <11201127+bgrnwd@users.noreply.github.com>
2025-04-06 08:02:51 +0000

    data: update standings

--- src/standings.py
+++ src/standings.py
@@ -169,7 +169,7 @@ with st.container():
         st.html(create_team_gt(admin))
 
 st.caption(
-    "Data is updated every Sunday, Monday, and Thursday morning. Last updated on Monday March 31, 2025 at 08:02:30 AM UTC."
+    "Data is updated every Sunday, Monday, and Thursday morning. Last updated on Sunday April 06, 2025 at 08:02:50 AM UTC."
 )
 st.caption(
     "The wordmarks, logos, trade names, packaging and designs of MLS, SUM, the current and former MLS member clubs are the exclusive property of MLS or their affiliates."
